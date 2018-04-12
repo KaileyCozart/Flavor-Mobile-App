954d87ed9c0971989c4470078f7f9b6e426f0135
KaileyCozart <kaileync@comcast.net>
2018-04-11 20:45:48 -0700

    Correctly Creates New Divs for Items

--- flavor/www/index.php
+++ flavor/www/index.php
@@ -45,7 +45,6 @@
                         <a href="search.html"><img id="icon" class="addPaddingRight" src="img/search.png" /></a>
                     </div>
                 </div>
-                <div id="entryItemOne" class="entry">
                     <?php
                         $connection = mysqli_connect('flavordb2.cohujudgkpgx.us-west-2.rds.amazonaws.com', 'kcozart21', 'my3e-mail', 'new_schema');
                         
@@ -55,57 +54,64 @@
 
                         $query = mysqli_query($connection,"SELECT recipe_name FROM recipe_test");
                         while ($row = mysqli_fetch_array($query)) {
+echo <<<TOP
+                            <div id="entryItemOne" class="entry">
+TOP;
                             echo "<h3>" . $row["recipe_name"] . "</h3>";
+                            
+echo <<<DIV
+                            <!--div id="deletePopup">
+                                <h3> Are you sure you want to delete this recipe? </h3>
+                                <div class="buttonDivWrapper">
+                                    <div class="buttonDiv">
+                                        <a href="index.html"><button> Yes </button></a>
+                                        <a href="index.html"><button> No </button></a>
+                                    </div>
+                                </div>
+                            </div-->
+                        <div class="buttonDivWrapper">
+                            <div class="buttonDiv">
+                                <a href="add.html"><button> Edit </button></a>
+                                <button id="deleteButton"> Delete </button>
+                            </div>
+                        </div>
+                        <div class="data">
+                            <h5> Ingredients </h5>
+                                <div class="ingredientAddWrapper addWrapper">
+                                    <ul class="ulIngredients">
+                                        <li>
+                                            <h6> Chicken </h6>
+                                            <h6> Wings </h6>
+                                        </li>
+                                    <ul>
+                                </div>
+                        </div>
+                        <div class="data">
+                            <h5> Instructions </h5>
+                            <div class="ingredientAddWrapper addWrapper">
+                                <ul class="ulInstruction">
+                                    <li>
+                                        <h6> 1. Do this... </h6>
+                                    </li>
+                                <ul>
+                            </div>
+                        </div>
+                        <div class="data">
+                            <h5> Tags </h5>
+                            <div class="ingredientAddWrapper addWrapper">
+                                <ul class="ulTags">
+                                    <li>
+                                        <h6> Dessert </h6>
+                                    </li>
+                                <ul>
+                            </div>
+                        </div>
+                    </div>
+DIV;
                         }
                         
                         mysqli_close($connection);
                     ?>
-                    <div id="deletePopup">
-                        <h3> Are you sure you want to delete this recipe? </h3>
-                        <div class="buttonDivWrapper">
-                        <div class="buttonDiv">
-                            <a href="index.html"><button> Yes </button></a>
-                            <a href="index.html"><button> No </button></a>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="buttonDivWrapper">
-                        <div class="buttonDiv">
-                            <a href="add.html"><button> Edit </button></a>
-                            <button id="deleteButton"> Delete </button>
-                        </div>
-                    </div>
-                    <div class="data">
-                        <h5> Ingredients </h5>
-                            <div class="ingredientAddWrapper addWrapper">
-                                <ul class="ulIngredients">
-                                    <li>
-                                        <h6> Chicken </h6>
-                                    </li>
-                                <ul>
-                            </div>
-                    </div>
-                    <div class="data">
-                        <h5> Instructions </h5>
-                        <div class="ingredientAddWrapper addWrapper">
-                            <ul class="ulInstruction">
-                                <li>
-                                    <h6> 1. Do this... </h6>
-                                </li>
-                            <ul>
-                        </div>
-                    </div>
-                    <div class="data">
-                        <h5> Tags </h5>
-                        <div class="ingredientAddWrapper addWrapper">
-                            <ul class="ulTags">
-                                <li>
-                                    <h6> Dessert </h6>
-                                </li>
-                            <ul>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <!--script type="text/javascript" src="cordova.js"></script-->
