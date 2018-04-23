678566a696f4857b35ec189c4184a4201abae804
KaileyCozart <kaileync@comcast.net>
2018-04-23 10:07:26 -0700

    Ajax Add to Db Working

--- flavor/www/add.php
+++ flavor/www/add.php
@@ -14,6 +14,57 @@
         <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
         <!-- Title -->
         <title>Flavor Mobile</title>
+        <script>
+                    var app = {
+                // Application Constructor
+                initialize: function () {
+                    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+                },
+
+                // deviceready Event Handler
+                //
+                // Bind any cordova events here. Common events are:
+                // 'pause', 'resume', etc.
+                onDeviceReady: function () {
+                    this.receivedEvent('deviceready');
+                },
+
+                // Update DOM on a Received Event
+                receivedEvent: function (id) {
+                    var parentElement = document.getElementById(id);
+                    var listeningElement = parentElement.querySelector('.listening');
+                    var receivedElement = parentElement.querySelector('.received');
+
+                    listeningElement.setAttribute('style', 'display:none;');
+                    receivedElement.setAttribute('style', 'display:block;');
+
+                    console.log('Received Event: ' + id);
+                }
+            };
+
+            app.initialize();
+
+
+            $(document).ready(function(e) {
+                $('#submit').click(function(){
+                    var name = $('#name').val();
+                    var ingredients = $('#ingredients').val();
+                    var instructions = $('#instructions').val();
+                    var tags = $('#tags').val();
+                    
+                    $.ajax({
+                        type:'POST',
+                        data:{name: name,ingredients: ingredients,instructions: instructions,tags: tags},
+                        url:"addentry.php", //php page URL where we post this data to save in databse
+                        success: function(data) {
+                            handleData(data);
+                            // html(data);
+                            window.location.href = "index.php";
+                        }
+                    })
+                });
+            });
+        </script>
     </head>
     <body>
         <div class="myApp">
@@ -29,9 +80,9 @@
                                         
                                     </li>
                                 <ul>
-                                <input type="text" class="input nameInput" placeholder="Enter Recipe Name" />
-                                <input type="submit" class="addButton" id="addNameButton" value="Add" />
-                                <input type="submit" class="addButton" id="editNameButton" value="Edit" />
+                                <input id="name" type="text" class="input nameInput" placeholder="Enter Recipe Name" />
+                                <!--input type="submit" class="addButton" id="addNameButton" value="Add" /-->
+                                <!--input type="submit" class="addButton" id="editNameButton" value="Edit" /-->
                             </div>
                     </div>
                     <div class="data">
@@ -42,8 +93,8 @@
                                         
                                     </li>
                                 <ul>
-                                <input type="text" class="input ingredientInput" placeholder="Enter Ingredient" />
-                                <input type="submit" class="addButton" id="addIngredientButton" value="Add" />
+                                <input id="ingredients" type="text" class="input ingredientInput" placeholder="Enter Ingredient" />
+                                <!--input type="submit" class="addButton" id="addIngredientButton" value="Add" /-->
                             </div>
                     </div>
                     <div class="data">
@@ -54,8 +105,8 @@
                                     
                                 </li>
                             <ul>
-                            <input type="text" class="input instructionInput" placeholder="Enter Instruction" />
-                            <input type="submit" class="addButton" id="addInstructionButton" value="Add" />
+                            <input id="instructions" type="text" class="input instructionInput" placeholder="Enter Instruction" />
+                            <!--input type="submit" class="addButton" id="addInstructionButton" value="Add" /-->
                         </div>
                     </div>
                     <div class="data">
@@ -66,14 +117,15 @@
                                     
                                 </li>
                             <ul>
-                            <input type="text" class="input tagInput" placeholder="Enter Tag" />
-                            <input type="submit" class="addButton" id="addTagButton" value="Add" />
+                            <input id="tags" type="text" class="input tagInput" placeholder="Enter Tag" />
+                            <!--input type="submit" class="addButton" id="addTagButton" value="Add" /-->
                         </div>
                     </div>
-                    <a href="index.html"><img id="addIcon" class="addPageAddIcon" src="img/add.png" /></a>
+                    <!-- onclick="send to sql" -->
+                    <a id="submit" type="submit" href="index.php"><img id="addIcon" class="addPageAddIcon" src="img/add.png" /></a>
                 </div>
             </div>
         </div>
-        <script type="text/javascript" src="js/index.js"></script>
+        <!--script type="text/javascript" src="js/index.js"></script-->
     </body>
 </html>
---
+++ flavor/www/addentry.php
@@ -0,0 +1,16 @@
+<?php
+$connection = mysqli_connect('flavordb2.cohujudgkpgx.us-west-2.rds.amazonaws.com', 'kcozart21', 'my3e-mail', 'new_schema');
+
+if($_REQUEST['name']){
+	$name = $_REQUEST['name'];
+	$ingredients = $_REQUEST['ingredients'];
+	$instructions= $_REQUEST['instructions'];
+	$tags = $_REQUEST['tags'];
+
+    $q = "INSERT INTO recipes VALUES (21,'$name', '$ingredients', '$instructions', '$tags')";
+    $query = mysqli_query($connection,$q);
+    echo "Data Inserted";
+}
+
+mysqli_close($connection);
+?>
